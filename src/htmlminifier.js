aedf505218d8eb33962a5d6499dcff0272bcceb9
Yuri Popov <yurikblech@gmail.com>
2025-05-30 11:22:55 +0300

    Fix A Regular Expression Denial of Service

--- src/htmlminifier.js
+++ src/htmlminifier.js
@@ -293,7 +293,7 @@ async function cleanAttributeValue(tag, attrName, attrValue, options, attrs) {
     attrValue = trimWhitespace(attrValue).split(/\s+,\s*|\s*,\s+/).map(function (candidate) {
       let url = candidate;
       let descriptor = '';
-      const match = candidate.match(/\s+([1-9][0-9]*w|[0-9]+(?:\.[0-9]+)?x)$/);
+      const match = candidate.match(/\s+((?:[1-9][0-9]*w)|(?:[0-9]+(?:\.[0-9]+)?x))$/);
       if (match) {
         url = url.slice(0, -match[0].length);
         const num = +match[1].slice(0, -1);
